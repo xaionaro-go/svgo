c2699a7770f7fdd3722d33443ee9bdc8fb069f50
Dmitry Yu Okunev <xaionaro@gmail.com>
2018-11-08 16:20:16 +0200

    Make possible to use RGB() for non "fill:"

--- svg.go
+++ svg.go
@@ -431,15 +431,15 @@ func (svg *SVG) Textlines(x, y int, s []string, size, spacing int, fill, align s
 
 // Colors
 
-// RGB specifies a fill color in terms of a (r)ed, (g)reen, (b)lue triple.
+// RGB specifies a color in terms of a (r)ed, (g)reen, (b)lue triple.
 // Standard reference: http://www.w3.org/TR/css3-color/
 func (svg *SVG) RGB(r int, g int, b int) string {
-	return fmt.Sprintf(`fill:rgb(%d,%d,%d)`, r, g, b)
+	return fmt.Sprintf(`rgb(%d,%d,%d)`, r, g, b)
 }
 
 // RGBA specifies a fill color in terms of a (r)ed, (g)reen, (b)lue triple and opacity.
 func (svg *SVG) RGBA(r int, g int, b int, a float64) string {
-	return fmt.Sprintf(`fill-opacity:%.2f; %s`, a, svg.RGB(r, g, b))
+	return fmt.Sprintf(`%s; fill-opacity:%.2f`, svg.RGB(r, g, b), a)
 }
 
 // Gradients
